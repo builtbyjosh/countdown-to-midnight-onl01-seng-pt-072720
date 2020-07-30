167847b2e299246739448ec50abdc60a84a880fd
Joshua Collins <pc.refuge@gmail.com>
2020-07-30 04:26:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,13 @@ def countdown(num)
   end
   return "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep(num)
+  while num > 0
+    sleep(1)
+    puts "#{num} SECOND(S)!"
+    num -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
